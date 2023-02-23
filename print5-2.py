16ad30ed8560442691d368123b4310ec3ab4471f
BeYoddy <changwoon4@naver.com>
2023-02-24 01:33:29 +0900

    input

---
+++ print5-2.py
@@ -0,0 +1,28 @@
+# name = input("enter your name :")
+# grade = input("enter your grade :")
+# com = input("enter your company name :")
+
+# print(name, grade, com)
+
+# 예제2
+# number = input("enter number :")
+# name = input("enter name :")
+
+# print("type of number", type(number), number *3)
+# print("type of name", type(name))
+
+# 예제3
+# first_number = int(input("enter number1 :"))
+# second_number = int(input("enter number2 :"))
+
+# total = first_number + second_number
+# print("first_number + second_number :", total)
+
+# 예제4
+float_number = float(input("enter a float number :"))
+
+print("input float : ", float_number)
+print("input type : ", type(float_number))
+
+print("firstName - {0}, LastName - {1}".format(
+    input("enter first name:"), input("enter second name")))
