87c7a6e164f8ed7e50b804e0ca81e667d2110961
BeYoddy <changwoon4@naver.com>
2023-03-24 23:58:36 +0900

    exception1

---
+++ print7-1.py
@@ -0,0 +1,21 @@
+# print('error)
+# print('error'))
+# if True
+#     pass
+# a = 10
+# b = 15
+# print(c)
+
+# ZeroDivisionError
+
+# print(100/ 0)
+
+# x = [50, 70, 90]
+# print(x[1])
+# print(x[4])
+
+# KeyError
+
+# dic = {'name' : 'Lee', 'Age' : 41, 'City' : 'Bus'}
+# print(dic['hobby'])
+# print(dic.get('hobby'))
