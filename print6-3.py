5ff8221b32bb5e80f3700cbcaca9c604b34df99f
BeYoddy <changwoon4@naver.com>
2023-03-24 22:43:36 +0900

    package3-1

---
+++ print6-3.py
@@ -0,0 +1,15 @@
+#예제1
+
+import sub.sub1.module1
+import sub.sub2.module2
+
+#사용
+sub.sub1.module1.mod1_test1()
+sub.sub1.module1.mod1_test2()
+
+sub.sub2.module2.mod2_test1()
+sub.sub2.module2.mod2_test2()
+
+print()
+print()
+print()
