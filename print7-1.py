eac20d365451645fdd770fa7975a25c322a2d60f
BeYoddy <changwoon4@naver.com>
2023-03-25 00:23:11 +0900

    exception2

--- print7-1.py
+++ print7-1.py
@@ -19,3 +19,51 @@
 # dic = {'name' : 'Lee', 'Age' : 41, 'City' : 'Bus'}
 # print(dic['hobby'])
 # print(dic.get('hobby'))
+
+# AttributeError
+import time
+# print(time.time2())
+
+# x = [10,50,90]
+# x.remove(50)
+# print(x)
+# x.remove(200)
+
+# FileNotFoundError
+
+# f = open('test.txt')
+
+# TypeError
+x = [1,2]
+y = (1,2)
+z = 'test'
+
+# print(x + y)
+# print(x + z)
+# print(y + z)
+print(x + list(y))
+print(x + list(z))
+
+name = ['Kim','Lee','Park']
+
+try:
+    z = 'Kim'
+    x = name.index(z)
+    print('Found it! {} in name'.format(z,x+1))
+except Exception:
+    print('Not found it! - Occurred ValueError!')
+else:
+    print('Ok! else.')
+
+print()
+
+try: 
+    a = 'park'
+    if a == 'kim':
+        print('ok! Pass')
+    else:
+        raise ValueError
+except ValueError:
+    print('Occurred! Exception!')
+else:
+    print('Ok! else!')
