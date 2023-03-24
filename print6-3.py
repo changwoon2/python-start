590941f5d0a9bf9443c1914e18441b5bece05225
BeYoddy <changwoon4@naver.com>
2023-03-24 22:57:11 +0900

    package2

--- print6-3.py
+++ print6-3.py
@@ -13,3 +13,24 @@ sub.sub2.module2.mod2_test2()
 print()
 print()
 print()
+
+from sub.sub1 import module1
+from sub.sub2 import module2 as m2
+
+module1.mod1_test1()
+module1.mod1_test2()
+
+m2.mod2_test1()
+m2.mod2_test2()
+
+print()
+print()
+print()
+
+from sub.sub1 import *
+from sub.sub2 import *
+
+module1.mod1_test1()
+module1.mod1_test2()
+module2.mod2_test1()
+module2.mod2_test2()
